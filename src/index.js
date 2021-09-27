c399f1f619340122c4fcb23825126c9232ccb43b
Le Trong Hieu <eotihvn@gmail.com>
2021-09-27 11:32:28 +0700

    leason routes

--- src/index.js
+++ src/index.js
@@ -2,29 +2,32 @@ const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
 const handlebars = require('express-handlebars');
+const route = require('./routes');
+
 const app = express();
 const port = 3000;
 
 // Cấp quyền cho phép người dùng có thể xem được những thứ trong folder public
 app.use(express.static(path.join(__dirname, 'public')));
 
-//HTTP logger
-app.use(morgan('combined'))
+app.use(express.urlencoded({
+    extended: true
+}))
+app.use(express.json()) // gửi từ code javascript
 
-//Template engine
+//HTTP logger
+// app.use(morgan('combined'))
+
+//Template engine ( đặt tên cho file)
 app.engine('hbs', handlebars({
     extname: ".hbs"
 }));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/', (req, res) => {
-    res.render('home');
-})
 
-app.get('/news', (req, res) => {
-    res.render('news');
-})
+//Routes init
+route(app);
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
